041f8229a523c0ee5225a2879f790337b3e2a71f
Maja <mj.gojska@gmail.com>
2022-03-15 14:59:01 +0100

    aaah display works

--- src/main/java/graphics/Display.java
+++ src/main/java/graphics/Display.java
@@ -13,6 +13,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.MeshView;
@@ -29,7 +30,7 @@ import java.util.EventListener;
 /**
  * fixme why hole disappears?
  */
-public class Display extends Application implements EventHandler {
+public class Display extends Application {
     public static Universe universe = Main.getUniverse();
 
     public static final int FRAME_WIDTH = 1100;
@@ -43,17 +44,15 @@ public class Display extends Application implements EventHandler {
     private SmartGroup root;
     private SmartGroup group;
 
+    private Pane displayPane;
+    private GridPane gridPane;
+
+
 
     @Override
     public void start(Stage stage) {
 
-        this.root = new SmartGroup();
         this.group = new SmartGroup();
-        root.getChildren().addAll(group);
-        Scene scene = new Scene(root, FRAME_WIDTH, FRAME_HEIGHT);
-        scene.setFill(Color.BLACK);
-
-        // add 3 different components for the mesh: grass, sandPits and water
         MeshView[] meshViews = universe.getMeshViews();
         for (MeshView meshView : meshViews) {
             this.group.getChildren().add(meshView);
@@ -61,8 +60,26 @@ public class Display extends Application implements EventHandler {
         this.group.getChildren().add(universe.getBall().getSphere());
         this.group.getChildren().add(universe.getTarget().getCircle());
 
+        SubScene subScene = new SubScene(this.group, FRAME_WIDTH - 200, FRAME_HEIGHT, true, SceneAntialiasing.BALANCED);
+        subScene.setFill(Color.BLACK);
+
         Camera camera = new PerspectiveCamera();
-        scene.setCamera(camera);
+
+        this.displayPane = new Pane();
+        displayPane.setPrefSize(FRAME_WIDTH, FRAME_HEIGHT);
+
+        this.gridPane = new GridPane();
+        addPanel();
+        this.displayPane.getChildren().add(this.gridPane);
+        gridPane.setTranslateX(FRAME_WIDTH - 200);
+        subScene.setCamera(camera);
+
+        Pane pane3D = new Pane();
+        pane3D.setPrefSize(FRAME_WIDTH - 200, FRAME_HEIGHT);
+        pane3D.getChildren().add(subScene);
+        this.displayPane.getChildren().add(pane3D);
+
+        Scene scene = new Scene(this.displayPane);
 
         // move the objects the middle
         group.translateXProperty().set(FRAME_WIDTH / 2.0);
@@ -75,25 +92,24 @@ public class Display extends Application implements EventHandler {
 
         scene.setOnKeyPressed(e -> {
             if (e.getCode() == KeyCode.W) {
-                takeShot(new Vector2D(5,5), stage);
+                takeShot(new Vector2D(5,5), stage, scene);
 
             }
         });
 
-        addPanel();
+
         stage.setScene(scene);
         stage.show();
     }
 
 
     private void addPanel() {
-        GridPane gridpane = new GridPane();
-        gridpane.setPrefSize(200, FRAME_HEIGHT);
-        gridpane.setStyle("-fx-background-color: darkgrey");
-        gridpane.setTranslateX(FRAME_WIDTH-200);
-        gridpane.setHgap(10);
-        gridpane.setVgap(10);
-        gridpane.setAlignment(Pos.CENTER);
+        gridPane.setPrefSize(200, FRAME_HEIGHT);
+        gridPane.setStyle("-fx-background-color: darkgrey");
+        gridPane.setTranslateX(FRAME_WIDTH-200);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setAlignment(Pos.CENTER);
 
         Font font = new Font("Verdana", 14);
         Font bigFont = new Font("Verdana", 20);
@@ -101,67 +117,66 @@ public class Display extends Application implements EventHandler {
         Text title = new Text("Golf Game \n- by group 6");
         title.setFill(Color.WHITE);
         title.setFont(bigFont);
-        gridpane.add(new HBox(30, title), 0, 0);
+        gridPane.add(new HBox(30, title), 0, 0);
 
         Text gap1 = new Text("");
-        gridpane.add(new HBox(30, gap1), 0, 1);
+        gridPane.add(new HBox(30, gap1), 0, 1);
 
-        Text text = new Text(" X-position:  " + universe.getBall().getPositionX() +
-                " \n Y-position:  " + universe.getBall().getPositionY() +
+        Text text = new Text(" X-position:  " + Math.round(universe.getBall().getPositionX())  +
+                " \n Y-position:  " + Math.round(universe.getBall().getPositionY()) +
                 " \n Number of shots: \n");
         text.setFill(Color.WHITE);
         text.setFont(font);
-        gridpane.add(new HBox(30, text), 0, 2);
+        gridPane.add(new HBox(30, text), 0, 2);
 
         Text gap2 = new Text("");
-        gridpane.add(new HBox(30, gap2), 0, 3);
+        gridPane.add(new HBox(30, gap2), 0, 3);
 
         Text readFromFile = new Text("Read file for\ninitial velocity");
         readFromFile.setFill(Color.WHITE);
         readFromFile.setFont(font);
-        gridpane.add(new HBox(30, readFromFile), 0, 4);
+        gridPane.add(new HBox(30, readFromFile), 0, 4);
 
         Button readFile = new Button("Read file");
-        gridpane.add(new HBox(30, readFile), 0, 5);
+        gridPane.add(new HBox(30, readFile), 0, 5);
         readFile.setOnMouseClicked(mouseEvent -> {
             universe.getFileReader().getNextShotFromFile();
         });
 
         Text gap3 = new Text("");
-        gridpane.add(new HBox(30, gap3), 0, 6);
+        gridPane.add(new HBox(30, gap3), 0, 6);
 
         Text readFromTextField = new Text("Type in\ninitial velocity");
         readFromTextField.setFill(Color.WHITE);
         readFromTextField.setFont(font);
-        gridpane.add(new HBox(30, readFromTextField), 0, 7);
+        gridPane.add(new HBox(30, readFromTextField), 0, 7);
 
         Text x = new Text("Velocity in x-direction:");
         x.setFill(Color.WHITE);
         x.setFont(font);
-        gridpane.add(new HBox(30, x), 0, 8);
+        gridPane.add(new HBox(30, x), 0, 8);
 
         TextField xvel = new TextField();
-        gridpane.add(new HBox(30, xvel), 0, 9);
+        gridPane.add(new HBox(30, xvel), 0, 9);
 
         Text y = new Text("Velocity in y-direction:");
         y.setFill(Color.WHITE);
         y.setFont(font);
-        gridpane.add(new HBox(30, y), 0, 10);
+        gridPane.add(new HBox(30, y), 0, 10);
 
         TextField yvel = new TextField();
-        gridpane.add(new HBox(30, yvel), 0, 11);
+        gridPane.add(new HBox(30, yvel), 0, 11);
 
         Button button = new Button("Hit the ball");
-        gridpane.add(new HBox(30, button), 0, 12);
+        gridPane.add(new HBox(30, button), 0, 12);
 
         button.setOnMouseClicked(mouseEvent -> {
             // TODO
         });
-        this.root.getChildren().add(gridpane);
     }
 
 
-    public void takeShot(Vector2D velocity, Stage stage) {
+    public void takeShot(Vector2D velocity, Stage stage, Scene scene) {
         universe.getBall().setVelocity(velocity);
         int counter = 0;
         while (universe.getBall().isMoving()) {
@@ -174,11 +189,6 @@ public class Display extends Application implements EventHandler {
 
 
         }
-//        stage.getScene().getWindow().setWidth(stage.getScene().getWidth() + Math.pow(-1, counter) + 13.5411111111);
     }
 
-    @Override
-    public void handle(Event event) {
-
-    }
 }
